26ebf00847c3e9f7193b5cac4b7b12326aa1eb8d
Will Nelson <will@wnelson.xyz>
2020-08-03 07:53:15 -0700

    Update presence structs

--- types/presence.go
+++ types/presence.go
@@ -1,26 +1,42 @@
 package types
 
-import "github.com/bwmarrin/snowflake"
+import (
+	"time"
+
+	"github.com/bwmarrin/snowflake"
+)
+
+// StatusUpdate represents the packet sent by the client to update its status
+type StatusUpdate struct {
+	Since  int64    `json:"since"` // Unix timestamp
+	Game   Activity `json:"activity,omitempty"`
+	Status string   `json:"status"`
+	AFK    bool     `json:"afk"`
+}
 
 // PresenceStatus represents a presence's status
 type PresenceStatus string
 
 // Presence statuses
 const (
-	PresenceStatusIdle    PresenceStatus = "idle"
-	PresenceStatusDND     PresenceStatus = "dnd"
-	PresenceStatusOnline  PresenceStatus = "online"
-	PresenceStatusOffline PresenceStatus = "offline"
+	PresenceStatusIdle      PresenceStatus = "idle"
+	PresenceStatusDND       PresenceStatus = "dnd"
+	PresenceStatusOnline    PresenceStatus = "online"
+	PresenceStatusOffline   PresenceStatus = "offline"
+	PresenceStatusInvisible PresenceStatus = "invisible"
 )
 
 // PresenceUpdate represents a presence update packet
 type PresenceUpdate struct {
-	User       User           `json:"user"`
-	Roles      []snowflake.ID `json:"roles"`
-	Game       Activity       `json:"activity"`
-	GuildID    snowflake.ID   `json:"guild_id"`
-	Status     PresenceStatus `json:"status"`
-	Activities []Activity     `json:"activities"`
+	User         User           `json:"user"`
+	Roles        []snowflake.ID `json:"roles"`
+	Game         Activity       `json:"activity"`
+	GuildID      snowflake.ID   `json:"guild_id"`
+	Status       PresenceStatus `json:"status"`
+	Activities   []Activity     `json:"activities"`
+	ClientStatus ClientStatus   `json:"client_status"`
+	PremiumSince time.Time      `json:"premium_since,omitempty"`
+	Nick         string         `json:"nick,omitempty"`
 }
 
 // ActivityType represents an activity's type
@@ -31,6 +47,7 @@ const (
 	ActivityTypeGame ActivityType = iota
 	ActivityTypeStreaming
 	ActivityTypeListening
+	ActivityTypeCustom
 )
 
 // ActivityFlag represents an activity's flags
@@ -48,18 +65,19 @@ const (
 
 // Activity represents an activity as sent as part of other packets
 type Activity struct {
-	Name          string       `json:"name"`
-	Type          ActivityType `json:"type"`
-	URL           string       `json:"url,omitempty"`
-	Timestamps    Timestamps   `json:"timestamps,omitempty"`
-	ApplicationID snowflake.ID `json:"application_id"`
-	Details       string       `json:"details,omitempty"`
-	State         string       `json:"state,omitempty"`
-	Party         Party        `json:"party,omitempty"`
-	Assets        Assets       `json:"assets,omitempty"`
-	Secrets       Secrets      `json:"secrets,omitempty"`
-	Instance      bool         `json:"instance,omitempty"`
-	Flags         ActivityFlag `json:"flags,omitempty"`
+	Name          string        `json:"name"`
+	Type          ActivityType  `json:"type"`
+	URL           string        `json:"url,omitempty"`
+	Timestamps    Timestamps    `json:"timestamps,omitempty"`
+	ApplicationID snowflake.ID  `json:"application_id"`
+	Details       string        `json:"details,omitempty"`
+	State         string        `json:"state,omitempty"`
+	Emoji         ActivityEmoji `json:"emoji,omitempty"`
+	Party         Party         `json:"party,omitempty"`
+	Assets        Assets        `json:"assets,omitempty"`
+	Secrets       Secrets       `json:"secrets,omitempty"`
+	Instance      bool          `json:"instance,omitempty"`
+	Flags         ActivityFlag  `json:"flags,omitempty"`
 }
 
 // Timestamps represents the starting and ending timestamp of an activity
@@ -87,6 +105,20 @@ type Secrets struct {
 	Join     string `json:"join,omitempty"`
 	Spectate string `json:"spectate,omitempty"`
 	Match    string `json:"match,omitempty"`
+}
+
+// ActivityEmoji represents the emoji shown in a custom status
+type ActivityEmoji struct {
+	Name     string       `json:"name"`
+	ID       snowflake.ID `json:"id,omitempty"`
+	Animated bool         `json:"animated,omitempty"`
+}
+
+// ClientStatus represents the client's status on each platform
+type ClientStatus struct {
+	Desktop PresenceStatus `json:"desktop,omitempty"`
+	Mobile  PresenceStatus `json:"mobile,omitempty"`
+	Web     PresenceStatus `json:"web,omitempty"`
 }
 
 // TypingStart represents a typing start packet
